150f02352927106911ff22534bd1606ca939766a
Anthony Ho <ahomnky11@gmail.com>
2025-02-08 21:18:51 +0800

    license

--- Package.swift
+++ Package.swift
@@ -4,44 +4,53 @@
 import PackageDescription
 
 let package = Package(
-  name: "espeak ",
-  products: [
-    .library(name: "libespeak-ng", targets: ["libespeak-ng"]),
-    .library(name: "espeak-ng-data", targets: ["data"]),
-  ],
-  targets: [
-    .target(
-      name: "libsonic",
-      exclude: [ "_repo" ],
-      cSettings: [
-      ]
-    ),
-    .target(
-      name: "libucd",
-      cSettings: [
-      ]
-    ),
-    .target(
-      name: "data",
-      resources: [
-        .copy("espeak-ng-data"),
-        .copy("phsource"),
-        .copy("dictsource"),
-      ]
-    ),
-    .target(
-      name: "libespeak-ng",
-      dependencies: [ "libsonic", "libucd" ],
-      exclude: [
-        "_repo",
-      ],
-      publicHeadersPath: "include",
-      cSettings: [
-        .headerSearchPath("."),
-        .headerSearchPath("_repo/src/include"),
-        .define("ESPEAK_NG_API", to: ""),
-        .define("N_PATH_HOME", to: "1024"),
-      ]
-    ),
-  ]
+    name: "espeak-ng",
+    products: [
+        .library(name: "libespeak-ng", targets: ["libespeak-ng"]),
+        .library(name: "espeak-ng-data", targets: ["data"]),
+    ],
+    targets: [
+        .target(
+            name: "libsonic",
+            exclude: [ "_repo" ],
+            cSettings: [
+                .headerSearchPath("."),
+                .headerSearchPath("../onnxruntime/include"), // existing onnxruntime path
+            ]
+        ),
+        .target(
+            name: "libucd",
+            cSettings: [
+                .headerSearchPath("."),
+                .headerSearchPath("../onnxruntime/include"), // existing onnxruntime path
+            ]
+        ),
+        .target(
+            name: "data",
+            resources: [
+                .copy("espeak-ng-data"),
+                .copy("phsource"),
+                .copy("dictsource"),
+            ]
+        ),
+        .target(
+            name: "libespeak-ng",
+            dependencies: [ "libsonic", "libucd" ],
+            exclude: [
+                "_repo",
+            ],
+            publicHeadersPath: "include",
+            cSettings: [
+                .headerSearchPath("."),
+                .headerSearchPath("_repo/src/include"),
+                .headerSearchPath("../onnxruntime/include"), // existing onnxruntime path
+                .define("ESPEAK_NG_API", to: ""),
+                .define("N_PATH_HOME", to: "1024"),
+            ]
+        ),
+    ]
 )
+
+$(SRCROOT)/voicenotes/Espeak/libespeak-ng/include
+$(SRCROOT)/voicenotes/espeak-ng/libespeak-ng/_repo/src/include
+$(SRCROOT)/YourApp/onnxruntime/include     // your existing onnxruntime path
